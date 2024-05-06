3cccb8cfcd3ae9a278bbc5b639b9db3eeb320370
Pedro A. De la Cruz <pedrodlcruz@coolhunterpartners.com>
2024-05-06 15:30:44 -0400

    Remove unused imports

--- app/src/main/java/com/example/tta/MainActivity.kt
+++ app/src/main/java/com/example/tta/MainActivity.kt
@@ -16,9 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.FloatingActionButton
